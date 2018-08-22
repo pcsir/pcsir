6db3d1c32f37c1612c306787de0b339b6d8dd9b6
sapatil10 <43saps@gmail.com>
2018-08-22 19:14:52 +0530

    Initial commit

---
+++ app/src/main/java/com/example/shubhankar/pcs_irsap/ChartsActivity.java
@@ -0,0 +1,85 @@
+package com.example.shubhankar.pcs_irsap;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class ChartsActivity extends AppCompatActivity {
+
+    boolean status = false;
+    Toolbar toolbar;
+    Spinner spinner;
+    ConfirmedFragment confirmedFragment;
+    WaitListFragment waitListFragment;
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_charts);
+
+        confirmedFragment = new ConfirmedFragment();
+        waitListFragment = new WaitListFragment();
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("Charts");
+
+        spinner = findViewById(R.id.spinner);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(ChartsActivity.this,
+                android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.chartType));
+
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(arrayAdapter);
+
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                Toast.makeText(ChartsActivity.this,
+                        spinner.getSelectedItem().toString(),
+                        Toast.LENGTH_SHORT)
+                        .show();
+
+                switch (position){
+                    case 0:
+                        setFragment(confirmedFragment);
+                        break;
+                    case 1:
+                        setFragment(waitListFragment);
+                        break;
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+    }
+
+    public void setFragment(Fragment fragment){
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.mainFrame,fragment);
+        fragmentTransaction.commit();
+    }
+}
+
+
