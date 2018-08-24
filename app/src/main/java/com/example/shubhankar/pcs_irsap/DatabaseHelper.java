c3c1c7b4eec7a0ffa3279e95d19f8557bebff0e7
Sap <pcsindianrail@gmail.com>
2018-08-24 21:04:31 +0530

    Database connection code written but not working

--- app/src/main/java/com/example/shubhankar/pcs_irsap/DatabaseHelper.java
+++ app/src/main/java/com/example/shubhankar/pcs_irsap/DatabaseHelper.java
@@ -21,7 +21,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public DatabaseHelper(Context context) {
         super(context, DB_NAME,null,1);
-        DB_PATH = context.getApplicationInfo().dataDir + "/databases";
+        DB_PATH = context.getApplicationInfo().dataDir + "/";
         this.context = context;
     }
 
